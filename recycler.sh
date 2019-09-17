c033122e221d3bb154134da26d2e24c3ae60779c
Manuel Hutter <manuel@vshn.ch>
2019-09-17 16:04:49 +0200

    Don't recycle volumes that are still in use
    If a PVC is deleted while the PV is still mounted by a pod, the recycler
    may clean up a PV and make it available again while it is still in use.
    This potentially leaks data to other users of the same cluster.
    
    This change adds a check that verifies the volume is not mounted
    anywhere before starting the cleanup process. If a volume is still
    mounted anywhere (as indicated by the number of fuse clients in `gluster
    volume status $vol client-list`), the process is aborted and the PV is
    left in the "Failed" phase.
    
    The recycler will retry to clean the volume on each invocation.
    
    Signed-off-by: Manuel Hutter <manuel@vshn.ch>

--- recycler.sh
+++ recycler.sh
@@ -254,6 +254,26 @@ recreate_volume() {
   return 0
 }
 
+# Verifies that no fuse clients are connected to the volume given
+#
+# $1 - list of gluster endpoints
+# $2 - name of the volume
+volume_is_used() {
+  local rhost="${1%%,*}"  # extract the first entry
+  local vol_name="$2"
+  nfuse="$(
+    gluster --remote-host="$rhost" --mode=script \
+      volume status "$vol_name" client-list | \
+    awk '/^fuse\s+[[:digit:]]+$/{print $2}')"
+
+  # if not mounted, nfuse should be empty (which is `-eq` to 0)
+  if [ "$nfuse" -eq 0 ]; then
+    return 0
+  fi
+
+  return 1
+}
+
 recycle_volume() {
   local volfile="$1"
   local vol_name
@@ -372,6 +392,12 @@ recycle_volume() {
     fi
   else
     echo 'Storage servers not specified' >&2
+    return 1
+  fi
+
+  # Check whether others are mounting this PV
+  if volume_is_used "$gluster_endpoints" "$vol_name"; then
+    echo "Volume $vol_name is mounted somewhere, skipping"
     return 1
   fi
 
